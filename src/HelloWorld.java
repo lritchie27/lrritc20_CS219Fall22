8bab35f17d8986c96554d7faf8231482bec377f6
lritc <lritchie277@gmail.com>
2022-08-30 14:56:22 -0400

    Today's Changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,13 @@
 public class HelloWorld {
 
     public static void main(String [] args) {
+        // These are print statements
         System.out.println("Hello World");
+        System.out.println("How are you?");
+
+        int x = 5;
+        boolean z = true;
+        double pi = 3.14159;
     }
 
 }
