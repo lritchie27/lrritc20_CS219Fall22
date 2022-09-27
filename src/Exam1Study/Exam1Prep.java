90b1ee35e9d9f864ff338e6a18502a99a358a46d
lritc <lritchie277@gmail.com>
2022-09-27 13:37:45 -0400

    Exam Practice

--- src/Exam1Study/Exam1Prep.java
+++ src/Exam1Study/Exam1Prep.java
@@ -7,10 +7,13 @@ public class Exam1Prep {
         String s = "hippopotomonstrosesquippedaliophobia";
         System.out.println(s.charAt(0)); // returns "h"
         System.out.println(s.indexOf('q')); // returns 19
+        System.out.println(s.indexOf('o', 5)); // returns 6
         System.out.println(s.indexOf('o')); // returns 4
         System.out.println(s.substring(s.indexOf('m'),16)); // returns "monstro"
-        System.out.println(s.charAt(s.length() - 1)); // if it was like original, returns indexOutOfBounds error
+        System.out.println(s.charAt(s.length() - 1)); // if it was like original (s.charAt(s.length())),
+                                                     // returns indexOutOfBounds error
 
+        System.out.println("");
 
         System.out.println(Math.round(6.02235e4)); // 6.02e4
         System.out.printf("%1.3f\n", 3.14159); // 3.142
