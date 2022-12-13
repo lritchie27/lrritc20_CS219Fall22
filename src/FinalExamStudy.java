d5415d236e77a337b4045e8d97b00fc218b186bc
lritc <lritchie277@gmail.com>
2022-12-13 00:11:52 -0500

    Final Exam Study

--- src/FinalExamStudy.java
+++ src/FinalExamStudy.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FinalExamStudy {
@@ -15,11 +16,60 @@ public class FinalExamStudy {
         //System.out.printf("%d %s\n", x, Arrays.toString(vec));  // array index out of bounds error
     //}
 
+    public static char toUpper(String s) {
+        char ch = s.charAt(0);
+        for (int i = 0; i < s.length(); i++) {
+            if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+                ch = (char) ((ch - 'a') + 'A');
+                break;
+            }
+        }
+        return ch;
+    }
+
+    public static ArrayList<Integer> twosum(ArrayList<Integer> nums, int target) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) == target) {
+                    result.add(i);
+                    result.add(j);
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(-32 % 7);  // -4
         System.out.println(Math.floorMod(-32, 7));  // 3
         System.out.println(2 / 4);  // 0
         System.out.println(Math.floorDiv(-2, 4)); // -1
+
+        System.out.println(Math.floorMod(-15, 12));
+
+        int ch = 'F';
+        ch = 'a' + (ch - 'A');
+        System.out.println(ch);  // 102
+        System.out.println((char) ch);  // f
+
+        int ch1 = 'f';
+        ch1 = (ch - 'a') + 'A';
+        System.out.println(ch1);  // 70
+        System.out.println((char) ch1);  // F
+
+        System.out.println(toUpper("hello"));
+        System.out.println(toUpper("HEllo"));
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(2);
+        nums.add(7);
+        nums.add(11);
+        nums.add(15);
+        System.out.println(nums);
+        System.out.println(twosum(nums, 17));
+
     }
 
 }
